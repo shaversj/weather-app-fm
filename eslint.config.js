8c0c6731d475ef5ce14dd1742dd421bf7b5bfeb9
Josh Shavers <shaversj@gmail.com>
2025-10-29 07:59:00 -0500

    Add header

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,38 @@
-//  @ts-check
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import perfectionist from "eslint-plugin-perfectionist";
 
-import { tanstackConfig } from '@tanstack/eslint-config'
-
-export default [...tanstackConfig]
+export default [
+    ...pluginRouter.configs["flat/recommended"],
+    perfectionist.configs["recommended-natural"],
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                project: "./tsconfig.json",
+                sourceType: "module",
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+        },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            "@typescript-eslint/only-throw-error": [
+                "error",
+                {
+                    allow: [
+                        {
+                            from: "package",
+                            name: "Redirect",
+                            package: "@tanstack/router-core",
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+];
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { createFileRoute } from "@tanstack/react-router";
+import logo from "/logo.svg";
+import units from "/icon-units.svg";
+import dropdown from "/icon-dropdown.svg";
+import { MenuButton, Menu } from "@headlessui/react";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: App,
-})
+});
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
+    // <!-- replaces: px-4 pt-4 md:px-6 md:pt-6 lg:px-28 lg:pt-12 -->
+    <div className="min-h-screen bg-neutral-900 px-[clamp(1rem,2vw+0.5rem,7rem)] pt-[clamp(1rem,1.5vw+0.5rem,3rem)]">
+      <header className={"flex justify-between"}>
+        <img src={logo} alt="Logo" />
+        <Menu>
+          <MenuButton className="inline-flex items-center gap-2 rounded-md bg-neutral-800 px-3 py-1.5 text-sm/6 font-semibold text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-700 data-open:bg-gray-700">
+            <img src={units} alt={"Units Icon"} />
+            Units
+            <img src={dropdown} alt={"Dropdown Icon"} />
+          </MenuButton>
+        </Menu>
       </header>
     </div>
-  )
+  );
 }
