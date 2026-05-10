789b0882f0baebc8bac49fea0328ac3ac5c65153
Josh Shavers <shaversj@gmail.com>
2026-05-10 14:22:39 -0500

    fix: separate search button from input container

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -217,22 +217,23 @@ function App() {
       <main className={""}>
         <div className={"flex flex-col items-center"}>
           <h1 className={"text-preset-2 mt-16 text-white"}>How's the sky looking today?</h1>
-          <div className={"relative mx-auto mt-16 w-full lg:w-[526px]"}>
-            <div className={"flex h-14 w-full items-center gap-4 rounded-xl bg-[#262540] px-4"}>
-              <img alt={"Search Icon"} className={"size-5"} src={search} />
-              <Input
-                className={"text-preset-5 placeholder:text-preset-5 h-6 w-full text-neutral-200 placeholder-neutral-200 outline-none"}
-                name={"Search"}
-                onChange={(e) => {
-                setSearchInput(e.target.value);
-                setShowCityDropdown(true);
-              }}
-                placeholder={"Search for a place..."}
-                type={"text"}
-                value={searchInput}
-              />
+          <div className={"relative mx-auto mt-16 flex w-full items-center gap-4 lg:w-[526px]"}>
+              <div className={"flex h-14 w-full items-center gap-4 rounded-xl bg-[#262540] px-4"}>
+                <img alt={"Search Icon"} className={"size-5"} src={search} />
+                <Input
+                  className={"text-preset-5 placeholder:text-preset-5 h-6 w-full text-neutral-200 placeholder-neutral-200 outline-none"}
+                  name={"Search"}
+                  onChange={(e) => {
+                  setSearchInput(e.target.value);
+                  setShowCityDropdown(true);
+                }}
+                  placeholder={"Search for a place..."}
+                  type={"text"}
+                  value={searchInput}
+                />
+              </div>
               <Button
-                className={"text-preset-5 flex h-10 shrink-0 rounded-xl bg-[#4658d9] px-4 text-neutral-200"}
+                className={"text-preset-5 flex h-14 shrink-0 rounded-xl bg-[#4658d9] px-4 text-neutral-200"}
                 disabled={!selectedLocation || isLoadingWeather}
                 onClick={() => {}}
               >
